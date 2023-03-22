2c68dfc927cbc2df384c3d2f20177b2f571b61c9
Davit <dav0709@icloud.com>
2023-03-22 23:05:43 +0400

    uxagreci

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int[] arr = new int[4];
+        Random rnd = new Random();
+        int sum = 0;
+
+    
+        for (int i = 0; i < arr.Length; i++) {
+            arr[i] = rnd.Next(-100, 100);
+            Console.Write(arr[i] + " ");
+        }
+
+        Console.WriteLine();
+
+        for (int i = 1; i < arr.Length; i += 2) {
+            sum += arr[i];
+        }
+
+        Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
+    }
+}
+
